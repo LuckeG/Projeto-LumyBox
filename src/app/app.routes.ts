5b4bfc8c47c72424ecb2b59247c3640aacdc7e05
LuckeG <lucas.gabriel01@aluno.ifce.edu.br>
2025-04-29 01:36:34 -0300

    nome bonitinho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,19 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './views/login/login.component';
+import { DefaultLoginLayoutComponent } from './components/default-login-layout/default-login-layout.component';
+import { ForgetPasswordComponent } from './pages/forget-password/forget-password.component'; // novo
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
-    {
-        path: "login",
-        component: LoginComponent
-    }
+    { path: "login", component: LoginComponent },
+    { path: '', component: DefaultLoginLayoutComponent},
+    { path: 'forgot-password', component: ForgetPasswordComponent},
+    { path: 'register', component: RegisterComponent },
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
